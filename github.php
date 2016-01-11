88e0460a9748a111414c1f9592050cc8301e1965
propaganja <herminioyatarjr@gmail.com>
2016-01-11 13:46:53 +0800

    test github.php script if bluehost is pulling from the github

--- github.php
+++ github.php
@@ -1,1 +1,36 @@
-<?php `git pull`;
+<?php // git pull;
+///////////////
+    ini_set('display_errors',1);
+    ini_set('display_startup_errors',1);
+    error_reporting(-1);
+//////////////
+
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/versebuster2/core/init.php';
+
+    $_db = DB::getInstance();
+//     $user = DB::getInstance()->query('SELECT * FROM members_tbl');
+
+//  echo "string";
+// // CHECKING OUTPUT
+// if(!$user->count()){
+//     echo "No user";
+// }else {
+//     // TO DISPLAY ALL
+//     foreach ($user->results() as $user) {
+//         echo $user->username , '<br>';
+//     } //END OF DISPLAY ALL
+
+//     echo $user->first()->username;//TO QUERY SINGLE DATA
+// }    
+
+// $userInsert = DB::getInstance()->insert('members_tbl', array(
+//     'fname' => 'zaq',
+//     'lname' => 'fdgsfsdfg',
+//     'userName' => 'fsd',
+//     'loginPassword' => 'sdf'
+//     ));    
+                $_db->insert('newsSubscriber_tbl', array(
+                    'news_name' => "akora",
+                    'news_email' => "akonEmailaddra@dfg.gf"
+                    ));
+?>
