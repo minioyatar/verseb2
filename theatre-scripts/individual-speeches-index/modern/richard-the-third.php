8fcf81f38640713a8a83ab5db84cc598f14f29ad
propaganja <herminioyatarjr@gmail.com>
2016-04-26 14:16:02 +0800

    test sample layout cataloguing

--- theatre-scripts/individual-speeches-index/modern/richard-the-third.php
+++ theatre-scripts/individual-speeches-index/modern/richard-the-third.php
@@ -8,15 +8,74 @@ require_once('127.0.0.1' == $_SERVER["REMOTE_ADDR"] ?  $_SERVER['DOCUMENT_ROOT']
 <!-- row -->	
 <div class="row main-text">
 	<div class="col-lg-9"><!-- #col-lg-9 introduction content -->
-		<div class="well pdf-header">
+		<div class="well ">
 			<p style="color:#b00000">MODERN</p>
 			<h3 class="page-title-ext text-center branding-style-reg" style="color:#b00000">The Tragedy of Richard the Third</h3>
-		</div>
+<!-- 		</div>
 
-		<div class="well ">
+		<div class="well "> -->
+              <div class="box">
+                <div class="box-header">
+                  <!-- <h3 class="box-title">Hover Data Table</h3> -->
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                      <tr style="background-color:#B00000;">
+                      	<th width=100 style="vertical-align: middle; text-align: center;border-color:#000;">speech's order in play's sequence</th>
+                      	<th width=200 style="vertical-align: middle; text-align: center;border-color:#000;">Name of Character</th>
+                        <th width=100 style="vertical-align: middle; text-align: center;border-color:#000;">Act</th>
+                        <th width=100 style="vertical-align: middle; text-align: center;border-color:#000;">Scene</th>                        
+                        <th style="vertical-align: middle; text-align: center; border-color:#000;">Opening line / famous line</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td><a href="">Richard</a></td>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td><a href="">Now is the winter of our discontent</a></td>
+
+                      </tr>
+                      <tr>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td><a href="">Richard</a></td>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td class="text-center"><a href="">2</a></td>
+                        <td><a href="">Was ever woman in this humour woo'd?</a></td>
+                      </tr>
+                      <tr>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td><a href="">Lady Anne</a></td>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td class="text-center"><a href="">2</a></td>
+                        <td><a href="">Set down, set your honourable load</a></td>
+                      </tr>
+                      <tr>
+                        <td class="text-center"><a href="">3</a></td>
+                        <td><a href="">Lady Anne</a></td>
+                        <td class="text-center"><a href="">1</a></td>
+                        <td class="text-center"><a href="">2</a></td>
+                        <td><a href="">What, do you tremble? are you all afraid?</a></td>
+                      </tr>
+                    </tbody>
+<!--                     <tfoot>
+                      <tr>
+                      	<th>speech's order in play's sequence</th>
+                      	<th>Name of Character</th>
+                        <th>Act</th>
+                        <th>Scene</th>                        
+                        <th>Opening line / famous line</th>
+                      </tr>
+                    </tfoot> -->
+                  </table>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->  
+
 
 			<?PHP
-				require_once(SITE_URL . "theatre-scripts/individual-speeches-index/tableTypes.php");
+				// require_once(SITE_URL . "theatre-scripts/individual-speeches-index/tableTypes.php");
 			?>	
 		</div>
 	</div><!-- ./ #col-lg-9 introduction content -->
